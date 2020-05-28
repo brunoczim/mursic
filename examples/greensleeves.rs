f91d02c264056b191012119286e7f8c4b6edf27d
brunoczim <brunoczim@gmail.com>
2020-05-28 01:41:18 -0300

    First part of greensleeves

--- examples/greensleeves.rs
+++ examples/greensleeves.rs
@@ -9,10 +9,87 @@ use mursic::{
 
 fn main() {
     let octave = 4;
-    let song = SongBuilder::default()
-        .bpm(NoteValue::Quarter, NaturalRatio::from(120))
+    let mut builder = SongBuilder::default();
+    builder.bpm(NoteValue::Quarter, NaturalRatio::from(120));
+    make_verse_first_part(octave, &mut builder);
+    builder
+        .pitch(Pitch { octave: octave + 1, key: Key::C })
+        .note_value(NoteValue::Half)
+        .note()
+        .pitch(Pitch { octave, key: Key::A })
+        .note_value(NoteValue::Quarter)
+        .note()
+        .compass()
+        .pitch(Pitch { octave, key: Key::A })
+        .dot(Dot::Single)
+        .note()
+        .pitch(Pitch { octave, key: Key::Ab })
+        .dot(Dot::None)
+        .note_value(NoteValue::Eighth)
+        .note()
+        .pitch(Pitch { octave, key: Key::A })
+        .note_value(NoteValue::Quarter)
+        .note()
+        .compass()
+        .pitch(Pitch { octave, key: Key::B })
+        .note_value(NoteValue::Half)
+        .note()
+        .pitch(Pitch { octave, key: Key::Ab })
+        .note_value(NoteValue::Quarter)
+        .note()
+        .compass()
+        .pitch(Pitch { octave, key: Key::E })
+        .note_value(NoteValue::Half)
+        .dot(Dot::Single)
+        .note()
+        .compass();
+
+    make_verse_first_part(octave, &mut builder);
+    builder
+        .pitch(Pitch { octave: octave + 1, key: Key::C })
+        .note_value(NoteValue::Quarter)
+        .dot(Dot::Single)
+        .note()
+        .pitch(Pitch { octave, key: Key::B })
+        .note_value(NoteValue::Eighth)
+        .dot(Dot::None)
+        .note()
+        .pitch(Pitch { octave, key: Key::A })
+        .note_value(NoteValue::Quarter)
+        .note()
+        .compass()
+        .pitch(Pitch { octave, key: Key::Ab })
+        .dot(Dot::Single)
+        .note()
+        .pitch(Pitch { octave, key: Key::Gb })
+        .dot(Dot::None)
+        .note_value(NoteValue::Eighth)
+        .note()
+        .pitch(Pitch { octave, key: Key::Ab })
+        .note_value(NoteValue::Quarter)
+        .note()
+        .compass()
+        .pitch(Pitch { octave, key: Key::A })
+        .note_value(NoteValue::Half)
+        .dot(Dot::Single)
+        .note()
+        .compass();
+
+    let song = builder.clear_finish();
+
+    let playable = PlayableSongBuilder::default()
+        .finish(song, SquareWaveBuilder::default());
+    let player = Player::new().unwrap();
+    player.play(playable);
+    player.wait();
+}
+
+fn make_verse_first_part(octave: u32, builder: &mut SongBuilder) {
+    builder
         .signature(TimeSignature { numer: 1, denom: NoteValue::Quarter })
         .pitch(Pitch { octave, key: Key::A })
+        .note_value(NoteValue::Quarter)
+        .dot(Dot::None)
         .note()
         .compass()
         .signature(TimeSignature { numer: 3, denom: NoteValue::Quarter })
@@ -51,41 +128,5 @@ fn main() {
         .pitch(Pitch { octave, key: Key::B })
         .note_value(NoteValue::Quarter)
         .note()
-        .compass()
-        .pitch(Pitch { octave: octave + 1, key: Key::C })
-        .note_value(NoteValue::Half)
-        .note()
-        .pitch(Pitch { octave, key: Key::A })
-        .note_value(NoteValue::Quarter)
-        .note()
-        .compass()
-        .pitch(Pitch { octave, key: Key::A })
-        .dot(Dot::Single)
-        .note()
-        .pitch(Pitch { octave, key: Key::Ab })
-        .dot(Dot::None)
-        .note_value(NoteValue::Eighth)
-        .note()
-        .pitch(Pitch { octave, key: Key::A })
-        .note_value(NoteValue::Quarter)
-        .note()
-        .compass()
-        .pitch(Pitch { octave, key: Key::B })
-        .note_value(NoteValue::Half)
-        .note()
-        .pitch(Pitch { octave, key: Key::Ab })
-        .note_value(NoteValue::Quarter)
-        .note()
-        .compass()
-        .pitch(Pitch { octave, key: Key::E })
-        .note_value(NoteValue::Half)
-        .dot(Dot::Single)
-        .note()
-        .compass()
-        .clear_finish();
-    let playable = PlayableSongBuilder::default()
-        .finish(song, SquareWaveBuilder::default());
-    let player = Player::new().unwrap();
-    player.play(playable);
-    player.wait();
+        .compass();
 }
