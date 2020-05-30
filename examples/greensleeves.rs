c9dde3e84acf1c7f34216a0cfdf8545c23dc1227
brunoczim <brunoczim@gmail.com>
2020-05-30 13:57:29 -0300

    Greensleeves example can save into a file

--- examples/greensleeves.rs
+++ examples/greensleeves.rs
@@ -3,12 +3,58 @@ use mursic::{
     num::NaturalRatio,
     pitch::{Key, Pitch},
     player::Player,
-    song::{PlayableSongBuilder, SongBuilder},
+    song::{PlayableSong, PlayableSongBuilder, SongBuilder},
+    source::Source,
     tempo::{Dot, NoteValue, TimeSignature},
-    wave::SawWaveBuilder,
+    wave::{SawWaveBuilder, Wave, WaveBuilder},
 };
+use std::{fs::File, path::PathBuf, process};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "greensleeves", about = "Play or save 8bit greensleeves")]
+enum Mode {
+    Play,
+    Save {
+        #[structopt(parse(from_os_str), default_value = "greensleeves.wav")]
+        file: PathBuf,
+    },
+}
+
+impl Mode {
+    fn run<W>(&self, song: PlayableSong<W>)
+    where
+        W: WaveBuilder + Send + Sync + 'static,
+        W::Source: Wave + 'static,
+    {
+        match self {
+            Mode::Play => {
+                let player = Player::new().unwrap();
+                player.play(song);
+                player.wait();
+            },
+
+            Mode::Save { file: path } => {
+                let file = match File::create(&path) {
+                    Ok(file) => file,
+                    Err(err) => {
+                        eprint!("{}: {}", path.display(), err);
+                        process::exit(-1);
+                    },
+                };
+
+                if let Err(err) = song.to_wav(file) {
+                    eprint!("{}: {}", path.display(), err);
+                    process::exit(-1);
+                }
+            },
+        }
+    }
+}
 
 fn main() {
+    let mode = Mode::from_args();
+
     let octave = 5;
     let mut builder = SongBuilder::default();
     builder
@@ -38,12 +84,10 @@ fn main() {
     make_second_outro(octave, &mut builder);
 
     let song = builder.clear_finish();
-
     let playable =
         PlayableSongBuilder::default().finish(song, SawWaveBuilder::default());
-    let player = Player::new().unwrap();
-    player.play(playable);
-    player.wait();
+
+    mode.run(playable);
 }
 
 fn make_verse_intro(octave: u32, builder: &mut SongBuilder) {
